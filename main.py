d39bff43f88a7ca89bc2f09d87041f13492ac28b
poojapbabar <babarpooja2002@gmail.com>
2024-05-13 20:31:25 +0530

    model_train

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from mlProject.config.configuration import ConfigurationManager
 from mlProject.pipeline.stage_01_Data_Ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_Data_Validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_Data_Transformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage_04_Model_Trainer import ModelTrainerTrainingPipeline
+# from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from mlProject import logger
 
 
@@ -37,3 +39,25 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+# STAGE_NAME = "Model evaluation stage"
+# try:
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+#    data_ingestion = ModelEvaluationTrainingPipeline()
+#    data_ingestion.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
